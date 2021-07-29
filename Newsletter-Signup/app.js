555c6b97c2de07806eefbb858aa7266fd278b720
rutuja <kadamrutuja103@gmail.com>
2021-07-29 17:51:08 +0530

    done

--- Newsletter-Signup/app.js
+++ Newsletter-Signup/app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const request = require("request");
 const bodyParser = require("body-parser");
+const https = require("https");
 
 const app = express();
 
@@ -12,12 +13,54 @@ app.get("/", function (req, res) {
 });
 
 app.post("/", function (req, res) {
-  var firstName = req.body.fname;
-  var lastName = req.body.lname;
-  var email = req.body.email;
-  console.log(firstName, lastName, email);
+  const firstName = req.body.fname;
+  const lastName = req.body.lname;
+  const email = req.body.email;
+
+  const data = {
+    members: [
+      {
+        email_address: email,
+        status: "subscribed",
+        merge_fields: {
+          FNAME: firstName,
+          LNAME: lastName,
+        },
+      },
+    ],
+  };
+
+  const jsonData = JSON.stringify(data);
+
+  const url = " https://us6.api.mailchimp.com/3.0/lists/85a7382ad2";
+
+  const options = {
+    method: "POST",
+    auth: "rutuja6:526f7aacf87165d04c67fabdeee87cb3-us6",
+  };
+
+  const request = https.request(url, options, function (response) {
+    if (response.statusCode === 200) {
+      res.sendFile(__dirname + "/success.html");
+    } else {
+      res.sendFile(__dirname + "/failure.html");
+    }
+
+    response.on("data", function (data) {
+      console.log(JSON.parse(data));
+    });
+  });
+
+  request.write(jsonData);
+  request.end();
 });
 
 app.listen(3000, function () {
   console.log("server is running on port 3000");
 });
+
+// /API Key
+// 526f7aacf87165d04c67fabdeee87cb3-us6
+
+// list id
+// 85a7382ad2
